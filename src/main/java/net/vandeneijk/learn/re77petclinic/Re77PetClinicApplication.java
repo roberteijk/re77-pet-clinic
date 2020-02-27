516fe3e952be8a603cb0eeea743e5c325e1910de
Robert van den Eijk <robert@vandeneijk.net>
2020-02-27 09:21:48 +0100

    Initial project add.

---
+++ src/main/java/net/vandeneijk/learn/re77petclinic/Re77PetClinicApplication.java
@@ -0,0 +1,13 @@
+package net.vandeneijk.learn.re77petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Re77PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Re77PetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/vandeneijk/learn/re77petclinic/Re77PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vandeneijk.learn.re77petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Re77PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
