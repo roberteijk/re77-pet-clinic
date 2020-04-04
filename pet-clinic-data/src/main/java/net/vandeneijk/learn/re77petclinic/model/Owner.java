333fdaa9e75b364251ba68cf9955aadfb4c987dd
Robert van den Eijk <robert@vandeneijk.net>
2020-04-04 09:03:43 +0200

    Added Visit and updated Owner with a Pet relationship. Closes #26.

--- pet-clinic-data/src/main/java/net/vandeneijk/learn/re77petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/net/vandeneijk/learn/re77petclinic/model/Owner.java
@@ -4,5 +4,17 @@
 
 package net.vandeneijk.learn.re77petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/net/vandeneijk/learn/re77petclinic/model/Visit.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Robert van den Eijk on 4-4-2020.
+ */
+
+package net.vandeneijk.learn.re77petclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
+
